36e421e4e51fec53319e7a6ef76cbc6698101963
shokoladber <allen.r.michael@gmail.com>
2022-02-16 19:38:40 -0600

    12.3.1. Add a Unique Id

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,11 +1,18 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event {
+
+    private int id;
+    private static int nextId = 1;
 
     private String name;
     private String description;
 
     public Event(String name, String description) {
+        this.id = nextId;
+        nextId++;
         this.name = name;
         this.description = description;
     }
@@ -24,6 +31,23 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
