72a291f6b03d8030022676706baeaf652bdea237
shokoladber <allen.r.michael@gmail.com>
2022-02-16 23:15:26 -0600

    12.3.5. Delete an Event

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -24,7 +24,7 @@ public class EventController {
 
     //lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String displayCreateEventForm(){
         return "events/create";
     }
 
@@ -33,6 +33,24 @@ public class EventController {
     public String createEvent(@RequestParam String eventName,
                               @RequestParam String eventDescription){
         EventData.add(new Event(eventName, eventDescription));
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String deleteEvent(@RequestParam(required = false) int[] eventIds){
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 
